8d8ba0451b92a28a076fa1e3d9110aed7ee75acb
hannah92.hailu@gmail.com <82427939+hannah-111@users.noreply.github.com>
2021-04-15 22:39:21 -0400

    commit first code

---
+++ Project 2/src/craftsoftware/HelloWorld.java
@@ -0,0 +1,17 @@
+package craftsoftware;
+
+public class HelloWorld {
+
+}
+public static void main(String[] args) {
+	// TODO Auto-generated method stub
+
+	String name = "Jamie Foxx";
+	double Salary = 2000;
+	double bonus = 100;
+	
+	System.out.println("Name of the Employee: " + name);
+	System.out.println("Salary of the Employee: " + Salary);
+	System.out.println("Bonus of the Employee: " + bonus);
+	
+}
