1cca5a802fcb7c04ca253ceade2aa8d5af6f39c5
hannah-111 <82427939+hannah-111@users.noreply.github.com>
2021-04-15 22:59:24 -0400

    Update HelloWorld.java

--- Project 2/src/craftsoftware/HelloWorld.java
+++ Project 2/src/craftsoftware/HelloWorld.java
@@ -7,7 +7,7 @@ public static void main(String[] args) {
 	// TODO Auto-generated method stub
 
 	String name = "Jamie Foxx";
-	double Salary = 2000;
+	double Salary = 2002;
 	double bonus = 100;
 	
 	System.out.println("Name of the Employee: " + name);
