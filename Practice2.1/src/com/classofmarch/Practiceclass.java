0c8b6d65875875275e929f5ed7be6311ddb04057
hannah-111 <hannah92.hailu@gmail.com>
2021-04-18 19:43:24 -0400

    please work

---
+++ Practice2.1/src/com/classofmarch/Practiceclass.java
@@ -0,0 +1,12 @@
+package com.classofmarch;
+
+public class Practiceclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World Selam Naw?"); 
+
+	}
+
+}
---
+++ Practice2.1/src/com/classofmarch2/Practicejava.java
@@ -0,0 +1,11 @@
+package com.classofmarch2;
+
+public class Practicejava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello This is Hewan, How are you all doing?");
+	}
+
+}
