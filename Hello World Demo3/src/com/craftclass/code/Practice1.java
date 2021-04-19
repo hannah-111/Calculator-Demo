96ac659321d80205ecb03ca2d40a658806f468f0
hannah-111 <hannah92.hailu@gmail.com>
2021-04-18 20:26:04 -0400

    Please commit

---
+++ Hello World Demo3/src/com/craftclass/code/Practice1.java
@@ -0,0 +1,11 @@
+package com.craftclass.code;
+
+public class Practice1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.print("Hello World selam Naw? from Hannah");
+	}
+
+}
